f8c91fdf82452443925b02a94904e352a8439f32
Dave Moten <davidmoten@gmail.com>
2017-11-25 06:23:32 +1100

    dev

--- src/main/java/com/github/davidmoten/apig/example/Handler.java
+++ src/main/java/com/github/davidmoten/apig/example/Handler.java
@@ -1,8 +1,12 @@
 package com.github.davidmoten.apig.example;
 
+import java.io.BufferedReader;
 import java.io.IOException;
+import java.io.InputStreamReader;
+import java.nio.charset.StandardCharsets;
 import java.util.Map;
 import java.util.concurrent.TimeUnit;
+import java.util.stream.Collectors;
 
 import org.kohsuke.github.GitHub;
 
@@ -10,6 +14,8 @@ import com.amazonaws.services.lambda.runtime.Context;
 import com.amazonaws.services.s3.AmazonS3;
 import com.amazonaws.services.s3.AmazonS3ClientBuilder;
 import com.amazonaws.services.s3.model.ListObjectsRequest;
+import com.amazonaws.services.s3.model.S3Object;
+import com.amazonaws.services.s3.model.S3ObjectInputStream;
 import com.github.davidmoten.aws.helper.StandardRequestBodyPassThrough;
 
 public class Handler {
@@ -23,36 +29,49 @@ public class Handler {
         String name = request.queryStringParameter("name")
                 .orElseThrow(() -> new IllegalArgumentException("parameter 'name' not found"));
 
+        String bucketName = System.getenv("BUCKET_NAME");
+        AmazonS3 s3 = AmazonS3ClientBuilder.defaultClient();
+        GitHub g;
+        try {
+            g = GitHub.connectAnonymously();
+        } catch (IOException e1) {
+            throw new RuntimeException(e1);
+        }
         if ("scheduled".equals(name)) {
-            String bucketName = System.getenv("BUCKET_NAME");
-            AmazonS3 s3 = AmazonS3ClientBuilder.defaultClient();
             long now = System.currentTimeMillis();
             int hourOfDay = hourOfDay(now);
             s3.listObjects(new ListObjectsRequest().withBucketName(bucketName).withMaxKeys(1000))
                     .getObjectSummaries() //
                     .stream() //
-                    .map(o -> {
+                    .forEach(o -> {
                         boolean expired = now - o.getLastModified().getTime() >= TimeUnit.HOURS
                                 .toMillis(24);
                         boolean isSlot = hourOfDay == Math.abs(o.getKey().hashCode()) % 24;
-                        int count;
                         if (expired && isSlot) {
                             try {
-                                GitHub g = GitHub.connectAnonymously();
-                                count = g.getRepository(o.getKey()).getStargazersCount();
+                                int count = g.getRepository(o.getKey()).getStargazersCount();
+                                s3.putObject(bucketName, o.getKey(), String.valueOf(count));
                             } catch (IOException e) {
                                 throw new RuntimeException(e);
                             }
-                            s3.putObject(bucketName, o.getKey(), String.valueOf(count));
-                            return count;
-                        } else {
-                            s3.getObject(bucketName, o.getKey()).getObjectContent();
-                            return 123;
                         }
                     });
             return "0";
+        } else if (s3.doesObjectExist(bucketName, name)) {
+            S3Object object = s3.getObject(bucketName, name);
+            return toString(object.getObjectContent());
         } else {
+            s3.putObject(bucketName, name, "0");
             return "0";
+        }
+    }
+
+    private static String toString(S3ObjectInputStream is) {
+        try (BufferedReader br = new BufferedReader(
+                new InputStreamReader(is, StandardCharsets.UTF_8))) {
+            return br.lines().collect(Collectors.joining("\n"));
+        } catch (IOException e) {
+            throw new RuntimeException();
         }
     }
 
